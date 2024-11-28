e995063ce03e397a77616d80889cffdbe09e69ca
ElenaErazo <152565202+ElenaErazo@users.noreply.github.com>
2024-11-28 17:00:37 -0600

    Update App.js

--- App.js
+++ App.js
@@ -16,5 +16,91 @@ export default function App() {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    <title>Evento Bad Bunny</title>
+  <style>
+    body 
+      font-family: Arial, sans-serif;
+      background-color: #f0f5ff;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    
+
+    .card {
+      background-color: #ffffff;
+      border-radius: 20px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      width: 300px;
+      overflow: hidden;
+      text-align: center;
+    }
+
+    .card img {
+      width: 100%;
+      height: auto;
+    }
+
+    .card-content {
+      padding: 20px;
+    }
+
+    .card-title {
+      font-size: 20px;
+      font-weight: bold;
+      margin: 10px 0;
+    }
+
+    .card-description {
+      font-size: 14px;
+      color: #555;
+      margin-bottom: 20px;
+    }
+
+    .card-details {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 10px;
+      color: #333;
+    }
+
+    .card-details span {
+      font-size: 14px;
+    }
+
+    .card-footer {
+      margin-top: 10px;
+      font-size: 14px;
+      font-weight: bold;
+      color: #007bff;
+    }
+
+    .icon {
+      font-size: 18px;
+      color: #007bff;
+      margin-right: 5px;
+    }
+  </style>
+<head/>
+<body>
+  <div class="card">
+    <img src="badbunny.webp" alt="Bad Bunny">
+    <div class="card-content">
+      <p class="card-title">Bad Bunny en El Salvador</p>
+      <p class="card-description">
+        La gira del artista puertorriqueño lo ha llevado a diferentes ciudades de Latinoamérica, este sábado finalmente será el turno de nuestro país.
+      </p>
+      <div class="card-details">
+        <span>📍 Estadio Cuscatlán</span>
+        <span>🗓️ Dom, 30 de Mar</span>
+      </div>
+      <div class="card-footer">Tickets disponibles!</div>
+    </div>
+  </div>
+</body>
+;
   },
 });
